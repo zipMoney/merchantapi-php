03758693d820506c928837015020726ff9256265
henryfan@zip <haolong.fan@zip.co>
2019-04-11 13:19:59 +1000

    update pipeline

--- build/shell/install.sh
+++ build/shell/install.sh
@@ -4,4 +4,4 @@
 ## Install for pipeline
 ##########################################################
 
- - apt-get update && apt-get install -y unzip git
+apt-get update && apt-get install -y unzip git
